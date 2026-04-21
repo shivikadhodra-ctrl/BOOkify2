95a86756fcdd34cd15ece2eff301d20bdb3d1201
Shivika <shivikadhodra@gmail.com>
2026-04-21 17:45:44 +0530

    deploy

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-import express from "express";
+/*import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js";
@@ -32,6 +32,52 @@ app.use("/api/user", userRouter);
 app.use("/api/hotels", hotelRouter);
 app.use("/api/rooms", roomRouter);
 app.use("/api/bookings", bookingRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));*/
+import path from "path";
+import { fileURLToPath } from "url";
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import connectDB from "./configs/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import userRouter from "./routes/userRoutes.js";
+import hotelRouter from "./routes/hotelRoutes.js";
+import roomRouter from "./routes/roomRoutes.js";
+import bookingRouter from "./routes/bookingRoutes.js";
+import clerkWebhooks from "./controllers/clerkWebhooks.js";
+import connectCloudinary from "./configs/cloudinary.js";
+import { stripeWebhooks } from "./controllers/stripeWebhooks.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+connectDB();
+connectCloudinary();
+
+const app = express();
+app.use(cors());
+
+// Stripe Webhook
+app.post("/api/stripe", express.raw({ type: "application/json" }), stripeWebhooks);
+
+app.use(express.json());
+app.use(clerkMiddleware());
+
+// Clerk Webhook
+app.use("/api/clerk", clerkWebhooks);
+
+app.use("/api/user", userRouter);
+app.use("/api/hotels", hotelRouter);
+app.use("/api/rooms", roomRouter);
+app.use("/api/bookings", bookingRouter);
+
+// Serve React frontend
+app.use(express.static(path.join(__dirname, "../client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
